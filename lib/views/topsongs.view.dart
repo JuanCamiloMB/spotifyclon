8891e4005eca9d7f24323dd4c0eb77052972a7fa
JuanK <juancamilo.manjarres@gmail.com>
2024-10-12 12:49:53 -0500

    fix: context and error

--- lib/views/topsongs.view.dart
+++ lib/views/topsongs.view.dart
@@ -2,16 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:spotifyclon/providers/topSongs.provider.dart';
 
-class TopSongs extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: TopSongsScreen(),
-    );
-  }
-}
-
-class TopSongsScreen extends ConsumerWidget {
+class TopSongs extends ConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     final topSongsAsyncValue = ref.watch(spotifySongsProvider);
@@ -20,52 +11,55 @@ class TopSongsScreen extends ConsumerWidget {
       appBar: AppBar(
         title: Text('Top 50 - Global'),
         leading: Navigator.canPop(context)
-        ? IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () {
-              Navigator.of(context).pop(); // Navega hacia atrás solo si es posible
-            },
-          )
-        : null,
+            ? IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () {
+                  Navigator.of(context)
+                      .pop(); // Navega hacia atrás solo si es posible
+                },
+              )
+            : null,
       ),
       body: topSongsAsyncValue.when(
         data: (data) {
-          return ListView.builder(
-            itemCount: data['tracks']['items'].length,
-            itemBuilder: (BuildContext context, int index) {
-              final song = data['tracks']['items'][index]['track'];
-              final songName = song['name'];
-              final artistName = song['artists'][0]['name'];
-              final albumArt = song['album']['images'][0]['url'];
-              return ListTile(
-                leading: Image.network(
-                  albumArt,
-                  width: 50,
-                  height: 50,
-                  fit: BoxFit.cover,
-                ),
-                title: Text(
-                  songName,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white, // Spotify-like style
+          if (data != null) {
+            return ListView.builder(
+              itemCount: data['tracks']['items'].length,
+              itemBuilder: (BuildContext context, int index) {
+                final song = data['tracks']['items'][index]['track'];
+                final songName = song['name'];
+                final artistName = song['artists'][0]['name'];
+                final albumArt = song['album']['images'][0]['url'];
+                return ListTile(
+                  leading: Image.network(
+                    albumArt,
+                    width: 50,
+                    height: 50,
+                    fit: BoxFit.cover,
                   ),
-                ),
-                subtitle: Text(
-                  artistName,
-                  style: TextStyle(color: Colors.grey),
-                ),
-                trailing: Icon(
-                  Icons.more_vert,
-                  color: Colors.white,
-                ),
-                tileColor: Colors.black, // Background color like Spotify
-                onTap: () {
-                  // Handle song tap (e.g., play song)
-                },
-              );
-            },
-          );
+                  title: Text(
+                    songName,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white, // Spotify-like style
+                    ),
+                  ),
+                  subtitle: Text(
+                    artistName,
+                    style: TextStyle(color: Colors.grey),
+                  ),
+                  trailing: Icon(
+                    Icons.more_vert,
+                    color: Colors.white,
+                  ),
+                  tileColor: Colors.black, // Background color like Spotify
+                  onTap: () {
+                    // Handle song tap (e.g., play song)
+                  },
+                );
+              },
+            );
+          }
         },
         loading: () =>
             Center(child: CircularProgressIndicator()), // Loading state
